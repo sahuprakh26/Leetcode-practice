1af47e2dd5fd56d18c54d46bd59efaa3b8b18bf7
sahuprakh26@gmail.com <sahuprakh26@gmail.com>
2021-10-30 23:17:40 +0530

    Function name changed

--- 104. Maximum Depth of Binary Tree.cpp
+++ 104. Maximum Depth of Binary Tree.cpp
@@ -9,7 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class  CheckIfSameTree {
 public:
     int maxDepth(TreeNode* root) {
         if(root==NULL)
