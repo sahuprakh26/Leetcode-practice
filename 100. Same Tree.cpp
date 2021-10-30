ccebf408c3bf2db808162f421c5140b34ca754e4
sahuprakh26@gmail.com <sahuprakh26@gmail.com>
2021-10-30 23:37:22 +0530

    function name change

--- 100. Same Tree.cpp
+++ 100. Same Tree.cpp
@@ -1,4 +1,4 @@
- /**
+/**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
@@ -9,11 +9,11 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
- class Solution {
+class Solution {
 public:
-   bool isSameTree(TreeNode *p, TreeNode *q) {
+   bool is_SameTree(TreeNode *p, TreeNode *q) {
     if (p == NULL || q == NULL) return (p == q);
-        return (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+        return (p->val == q->val && is_SameTree(p->left, q->left) && is_SameTree(p->right, q->right));
     }
 };
 
